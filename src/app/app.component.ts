59fbf7b4a50fe0601ff7baad3cbba57414b107fc
= <=>
2021-11-01 13:53:03 +1100

    Initial features completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
 
+interface AccountType {
+  value: string;
+  viewValue: string;
+}
+
+interface NumberType {
+  value: number;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ips-test';
+  title = 'IPS Test - Matthew Clifford';
+
+  personNumber: number = 0;
+  companyNumber: number = 0;
+  account: string = '';
+  personArray: number[] = [];
+  companyArray: number[] = [];
+
+  accounts: AccountType[] = [
+    { value: 'personal', viewValue: 'Personal' },
+    { value: 'corporate', viewValue: 'Corporate' },
+  ];
+
+  numbers: NumberType[] = [
+    { value: 1, viewValue: '1' },
+    { value: 2, viewValue: '2' },
+    { value: 3, viewValue: '3' },
+    { value: 4, viewValue: '4' },
+    { value: 5, viewValue: '5' },
+    { value: 6, viewValue: '6' },
+  ];
+
+  companyNumbers: NumberType[] = [
+    { value: 1, viewValue: '1' },
+    { value: 2, viewValue: '2' },
+    { value: 3, viewValue: '3' },
+    { value: 4, viewValue: '4' },
+    { value: 5, viewValue: '5' },
+    { value: 6, viewValue: '6' },
+  ];
+
+  changeNumber(value: any): void {
+    this.personNumber = value;
+    console.log(this.personNumber);
+  }
+
+  changeCompanyNumber(value: any): void {
+    this.companyNumber = value;
+    console.log(this.companyNumber);
+  }
+
+  changeAccount(value: any): void {
+    this.account = value;
+    console.log(this.account);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
